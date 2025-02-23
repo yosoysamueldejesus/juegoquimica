2fde9359c052edcb8b651bdbd1eee49ce92b92bc
Samuel de Jesus <yosoysamueldejesus@gmail.com>
2025-02-23 12:33:32 -0400

    mapa y scrip de personaje

--- scripst/player.gd
+++ scripst/player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -350.0
 
 
-func _physics_process(delta):
+
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -17,6 +21,22 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction > 0:
+		animated_sprite_.flip_h = false 
+	elif direction < 0:
+		animated_sprite_.flip_h = true 
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_.play("quieto")
+
+
+
+
+
+
+
+
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
